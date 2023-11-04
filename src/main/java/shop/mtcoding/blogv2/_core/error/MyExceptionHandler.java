21e0dfeed061e19cb97ba685069655fced97e622
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-04 17:15:33 +0900

    feat: add 401 Exception

--- src/main/java/shop/mtcoding/blogv2/_core/error/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/blogv2/_core/error/MyExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blogv2._core.error.ex.Exception400;
+import shop.mtcoding.blogv2._core.error.ex.Exception401;
 import shop.mtcoding.blogv2._core.util.Script;
 
 @ControllerAdvice
@@ -12,6 +13,13 @@ public class MyExceptionHandler {
     @ResponseBody
     @ExceptionHandler(Exception400.class)
     public String ex400(Exception400 e){
+        return Script.back(e.getMessage());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception401.class)
+    public String ex401(Exception401 e) {
+        // 코드 ex) 로그인 횟수 초과 시 로직 처리
         return Script.back(e.getMessage());
     }
 }
